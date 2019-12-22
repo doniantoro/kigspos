65c6d838443f02324e947c2792dfed4f76c53d89
ToyibN <toyibseha@gmail.com>
2019-12-22 23:21:52 +0700

    authentication

--- app/User.php
+++ app/User.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
     protected $table = 'user';
     protected $fillable = ['name','username', 'password'];
@@ -17,4 +19,5 @@ class User extends Model
     public function user_level(){
         return $this->hasMany('App\UserLevel', 'user_level_id');
     }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 //Create different route file for each feature inside /routes/demo_routes folder
-//Create the route with the feature name
-//Include the route here
-include('demo_routes/pos.php');
-include('demo_routes/category.php');
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,24 +13,40 @@ include('demo_routes/category.php');
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('login'));
 //    return view('dashboard/index');
 });
 
-Route::get('/produk/manual_input', 'InventoryController@manual_input');
-Route::get('/produk/manual_input_proses', 'InventoryController@manual_input_proses');
-Route::post('/InventoryProduct/input_excell', 'InventoryController@input_excell');
+Route::group(['middleware' => ['auth']], function () {
+
+//Create the route with the feature name
+//Include the route here
+    include('demo_routes/pos.php');
+    include('demo_routes/category.php');
+
+    Route::get('/produk/manual_input', 'InventoryController@manual_input');
+    Route::get('/produk/manual_input_proses', 'InventoryController@manual_input_proses');
+    Route::post('/InventoryProduct/input_excell', 'InventoryController@input_excell');
 
 
-//goods_stock
-Route::get('produk/goods_stock', 'InventoryController@goods_stock');
-Route::get('produk/update_stock/{sku}', 'InventoryController@update_stock');
-Route::get('produk/delete_stock/{sku}', 'InventoryController@delete_stock');
-Route::get('produk/return_stock/{sku}', 'InventoryController@return_stock');
-Route::get('/produk/update_stock_proses/{sku}', 'InventoryController@update_stock_proses');
+    //goods_stock
+    Route::get('produk/goods_stock', 'InventoryController@goods_stock');
+    Route::get('produk/update_stock/{sku}', 'InventoryController@update_stock');
+    Route::get('produk/delete_stock/{sku}', 'InventoryController@delete_stock');
+    Route::get('produk/return_stock/{sku}', 'InventoryController@return_stock');
+    Route::get('/produk/update_stock_proses/{sku}', 'InventoryController@update_stock_proses');
 
 
 
 //flow barang
 Route::get('produk/flow_barang', 'InventoryController@flow_barang');
+});
 
+Route::get('logout', 'Auth\LoginController@logout', function () {
+    return abort(404);
+});
+
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
